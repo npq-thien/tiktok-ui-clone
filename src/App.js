2aea1491ca4dc2db0127e7ac7a7d5f425e1eae93
npq-thien <thiennguyen3132002@gmail.com>
2023-10-22 21:45:19 +0700

    babel-plugin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Button from '~/components/Button'
 
 function App() {
   console.log('hello world');
   return (
     <div className="App">
+      <Button />
       <h1>Hello react</h1>
     </div>
   );
