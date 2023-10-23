9246bea4a590d80a51f21bce9319c596080e1f1f
npq-thien <thiennguyen3132002@gmail.com>
2023-10-23 18:00:00 +0700

    router/Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
+import { Fragment } from 'react';
 import './App.css';
-import Button from '~/components/Button';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { publicRoutes } from '~/routes';
+import { DefaultLayout, HeaderOnly } from './components/Layout/';
 
 function App() {
-    console.log('hello world');
     return (
-        <div className="App">
-            <Button />
-            <h1>Hello react</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
