8c601937d492b7070986c82eb9547a6dc9038442
Thien <thiennguyen3132002@gmail.com>
2023-10-23 11:33:28 +0700

    prettier

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import Button from '~/components/Button'
+import Button from '~/components/Button';
 
 function App() {
-  console.log('hello world');
-  return (
-    <div className="App">
-      <Button />
-      <h1>Hello react</h1>
-    </div>
-  );
+    console.log('hello world');
+    return (
+        <div className="App">
+            <Button />
+            <h1>Hello react</h1>
+        </div>
+    );
 }
 
 export default App;
