44f052eaa1c4ac3e9c7a8679ff831c140670332c
npq-thien <thiennguyen3132002@gmail.com>
2023-11-07 22:02:38 +0700

    header#2

---
+++ src/components/Layout/AccountItem/index.js
@@ -0,0 +1,28 @@
+import { Wrapper } from '~/components/Popper';
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/8d9d2ae58519e1560dfc386b72f1bbd2.jpeg?x-expires=1699534800&x-signature=42jAuSMDM%2Bkw0RNYC%2FOZWRqbqbM%3D"
+                alt="Hoa"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check-icon')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
